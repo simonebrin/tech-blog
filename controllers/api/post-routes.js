6b1204f46b8dd1aca424e2c9d3808240d217c56c
Simone Brin <simonebrin@gmail.com>
2022-11-14 10:07:57 -0500

    started figuring out connection issues and added post-routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
+const hasAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'title', 'post_text'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text']
+            },
+            {
+                model: User,
+                attributes: ['id', 'name']
+            }
+        ]
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
