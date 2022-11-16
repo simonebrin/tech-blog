e8e51da90efaa6c3d6a687c174ddc61659d2558f
Simone Brin <simonebrin@gmail.com>
2022-11-15 21:27:54 -0500

    submit

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,21 +2,21 @@ const router = require('express').Router();
 const { User } = require('../../models');
 const hasAuth = require('../../utils/auth');
 
-router.post('/', (req, res) => {
-    User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-    })
-        .then((dbUserData) => {
-            req.session.userId = dbUserData.id;
-            res.json(dbUserData);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
+// router.post('/', (req, res) => {
+//     User.create({
+//         username: req.body.username,
+//         email: req.body.email,
+//         password: req.body.password,
+//     })
+//         .then((dbUserData) => {
+//             req.session.userId = dbUserData.id;
+//             res.json(dbUserData);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         });
+// });
 
 router.post('/', (req, res) => {
     User.create({
